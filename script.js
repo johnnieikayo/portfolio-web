e5bda9f113f93e7edc3a15c030153915cb4f1354
johnnieikayo <ikayojohn4@gmail.com>
2025-05-13 15:00:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,4 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+document.getElementById("toggle-dark").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
 
